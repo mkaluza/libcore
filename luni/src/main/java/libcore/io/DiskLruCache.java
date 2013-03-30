3fa8c42c9a154e1041cc89bfd8e71ea393317056
jwilson <jwilson@squareup.com>
2013-03-30 17:49:33 -0400

    Automatically flush the cache when an edit is completed.
    This will remove the need for applications to manually flush
    the cache.
    
    Change-Id: I6658634f2e6a333c486a5eb5144e7989d3c50be8

--- luni/src/main/java/libcore/io/DiskLruCache.java
+++ luni/src/main/java/libcore/io/DiskLruCache.java
@@ -509,6 +509,7 @@ public final class DiskLruCache implements Closeable {
             lruEntries.remove(entry.key);
             journalWriter.write(REMOVE + ' ' + entry.key + '\n');
         }
+        journalWriter.flush();
 
         if (size > maxSize || journalRebuildRequired()) {
             executorService.submit(cleanupCallable);
